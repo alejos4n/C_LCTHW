d0f501d2925b5d238cdd5ebfe8ac953e0198cf2c
Alejandro Sánchez <23413433+alejos4n@users.noreply.github.com>
2025-01-24 01:07:42 -0500

    Se agregan comentarios y tipos de comentarios
    Se explica cómo funciona la salida %d

--- ex1.c
+++ ex1.c
@@ -3,7 +3,10 @@
 /* esta es una forma de comentario */
 int main(int argc, char*argv[])
 {
+	//otro comentario
+	// inicializo la variable distance
 	int distance = 100;
-	printf("usted está %d km de.\n",distance);
+	printf("usted está %d km de distancia", distance);
+	// con %d y ,distance; estoy indicando que en el %d irá el valor de la variable distance
 	return 0;
 }
