c6cfc498a0d0602577dfbf08a6cb530aed06e9df
Alejandro Sánchez <23413433+alejos4n@users.noreply.github.com>
2025-01-25 00:46:50 -0500

    imprimiendo algunos valores

--- exercise3.c
+++ exercise3.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+//int main(int argc, char *argv[0])
+//en la linea anterior nos pedirá argumentos por consola al ejecutar el programa.
 int main()
 {
-    int age = 10;
+    int age = 15;
     int height = 72;
 
     printf("Yo tengo %d años en IT\n",age);
-    printf("y tengo %d servidores\n",height);
+    printf("y administro %d servidores\n",height);
 
     return 0;
 }
-
