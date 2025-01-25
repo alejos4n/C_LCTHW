278f2c0c296ba1e8d353a592d25082191d577c2e
Alejandro Sánchez <23413433+alejos4n@users.noreply.github.com>
2025-01-25 01:39:17 -0500

    notas sobre operadores

---
+++ 5exercise.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+operadores aritmeticos:
++suma
+-resta
+*multi
+/divi
+%modulus
+++incremento
+--decremento
+==igual
+!= not igual
+> mayor que, greater than
+< less than menor que
+>= mayor o igual, greater than iqual
+<= menor igual, less than equal
+&& logical and
+|| logical or
+! logical not
+? : logical ternary
+
+*/
